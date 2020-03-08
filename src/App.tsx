96598c4daa726715a1b72828970490d809c91099
euxn23 <euxn23@gmail.com>
2020-03-09 01:00:47 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const playerRef = useRef<any>(null);
+  const loadedRef = useRef<any>(false);
+
+  const handleClick = () => {
+    if (!(playerRef.current && loadedRef.current)) {
+      return;
+    }
+
+    playerRef.current.seekTo(3730);
+  };
+
+  useEffect(() => {
+    const ytScriptTag = document.createElement('script');
+    ytScriptTag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(ytScriptTag);
+
+    const onPlayerReady = () => {
+      loadedRef.current = true;
+    };
+
+    (window as any).onYouTubeIframeAPIReady = () => {
+      const { YT } = window as any;
+      playerRef.current = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'M5zxb_3d2FA',
+        startSeconds: 3730,
+        events: {
+          onReady: onPlayerReady
+        }
+      });
+    };
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>再生</button>
+      <button>合いの手を入れる</button>
     </div>
   );
 }
